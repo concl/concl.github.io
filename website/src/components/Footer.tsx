fa1b9d33c40a74584514dcab1524f3cc4c523adc
LukeZengf <bonumconcl@gmail.com>
2025-12-21 16:59:16 -0800

    Update Footer.tsx

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -13,6 +13,16 @@ function Footer() {
                 <a href="https://github.com/concl" target="_blank" rel="noopener noreferrer">@concl</a>
             </div>
             <div className="Link">
+                <p>Itch</p>
+                <hr />
+                <a href="https://concl.itch.io" target="_blank" rel="noopener noreferrer">@concl</a>
+            </div>
+            <div className="Link">
+                <p>LinkedIn</p>
+                <hr />
+                <a href="https://www.linkedin.com/in/luke-zeng/" target="_blank" rel="noopener noreferrer">Luke Zeng</a>
+            </div>
+            <div className="Link">
                 <p>Email</p>
                 <hr />
                 <a href="mailto:zenglukef@gmail.com" target="_blank" rel="noopener noreferrer">zenglukef@gmail.com</a>
