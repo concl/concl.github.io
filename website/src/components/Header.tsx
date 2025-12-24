881d49015e5eb210329059beff0ef98de2e13036
LukeZengf <bonumconcl@gmail.com>
2025-12-23 21:29:05 -0800

    header modificaiton

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,15 +1,35 @@
 import "./Header.css";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Header() {
+
+    const [tag, setTag] = useState("");
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setTag("");
+        } else {
+            if (location.pathname.startsWith("/blog")) {
+                setTag(" — Blog");
+                return;
+            } else if (location.pathname.startsWith("/cv")) {
+                setTag(" — CV");
+                return;
+            }
+            setTag(" — " + location.pathname.slice(1).charAt(0).toUpperCase() + location.pathname.slice(2));
+        }
+    }, [location]);
+    
     return (
         <header className="header">
             <nav className="nav">
                 <div className="name">
-                    <NavLink to="/">Luke Zeng</NavLink>
+                    <NavLink to="/">Luke Zeng<div className="tag">{tag}</div></NavLink>
                 </div>
-                <NavLink
+                {/* <NavLink
                     to="/"
                     className={({ isActive }) =>
                         isActive ? "nav-link active" : "nav-link"
@@ -17,7 +37,7 @@ function Header() {
                     end
                 >
                     Home
-                </NavLink>
+                </NavLink> */}
                 <NavLink
                     to="/blog"
                     className={({ isActive }) =>
